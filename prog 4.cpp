511a206bcbc06894e784b312af1cee2297cdc043
mounick1 <122512394+mounick1@users.noreply.github.com>
2024-01-05 12:22:50 +0530

    Add files via upload

---
+++ prog 4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Addition: " << num1 + num2 << endl;
+    cout << "Subtraction: " << num1 - num2 << endl;
+    cout << "Multiplication: " << num1 * num2 << endl;
+    cout << "Division: " << num1 / num2 << endl;
+
+    return 0;
+}
